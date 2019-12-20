12e74540c5421e687e8724c71c4a423199cf0b7d
Ahmed Ramadan <8378485+midosh@users.noreply.github.com>
2019-12-20 03:44:57 +0200

    project redy for deploy on EB

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramdbdev",
+    "password": "udagramdbdev",
+    "database": "udagramdbdev",
+    "host": "udagramdbdev.ck7qfydjxsr1.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-ahmed-dev"
   },
   "prod": {
     "username": "",
