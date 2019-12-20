66489711ebb38c39a3dcd14b9f48c4e3489a47b7
Ahmed Ramadan <8378485+midosh@users.noreply.github.com>
2019-12-20 04:27:30 +0200

    update src/aws.ts file add deploy profile

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -6,7 +6,7 @@ export const config = {
     "host": "udagramdbdev.ck7qfydjxsr1.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-1",
-    "aws_profile": "default",
+    "aws_profile": "DEPLOYED",
     "aws_media_bucket": "udagram-ahmed-dev"
   },
   "prod": {
